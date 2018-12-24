a1a1183a4a8f88d4450cc83712f5d5563cf486fb
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-12-24 12:25:18 +0000

    Add (common) types and platform types headers

---
+++ include/konbini/platformtypes.hpp
@@ -0,0 +1,10 @@
+/// @file
+
+#pragma once
+
+#ifdef _WIN32
+#	define WIN32_LEAN_AND_MEAN
+#	include <windows.h>
+#else
+#	include <dlfcn.h>
+#endif
---
+++ include/konbini/types.hpp
@@ -0,0 +1,14 @@
+/// @file
+
+#pragma once
+
+#include <string>
+
+#include "platformtypes.hpp"
+
+namespace konbini
+{
+
+using tString = std::string;
+
+}; // namespace konbini
